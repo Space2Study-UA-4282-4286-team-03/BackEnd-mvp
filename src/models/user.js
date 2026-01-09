939a7b455f6378997ee450420465e1b25f5a2369
Gennadiy Naumovych <navmovn@gmail.com>
2026-01-09 15:59:39 +0200

    Fix - auth.js add original error text in googleLogin function. File user.js - allow null for nativeLanguage enum. File auth.yaml add security schemes to OpenAPI spec. File serverSetup.js validate required environment variables at startup

--- src/models/user.js
+++ src/models/user.js
@@ -86,7 +86,7 @@ const userSchema = new Schema(
     nativeLanguage: {
       type: String,
       enum: {
-        values: SPOKEN_LANG_ENUM,
+        values: [...SPOKEN_LANG_ENUM, null],
         message: ENUM_CAN_BE_ONE_OF('native language', SPOKEN_LANG_ENUM)
       },
       default: null
