4f0375a62ae361f987ce0fe1543b62f071961394
AnnaPannaCotta <an.vasilyeva99@gmail.com>
2026-01-21 21:46:17 +0200

    #62-(My Resources) fix category.map is not a function error
    Co-authored-by: Anna <hitrichanna@icloud.com>

--- src/utils/getCategoriesOption.js
+++ src/utils/getCategoriesOption.js
@@ -1,8 +1,30 @@
+const mongoose = require('mongoose')
+
 const getCategoriesOptions = (categories) => {
-  if (categories) {
-    return categories.map((item) => (item === 'null' ? null : item))
-  } else {
-    return
+  if (!categories) return undefined
+
+  const normalize = (cat) => {
+    if (typeof cat === 'object') {
+      if (!cat.value || cat.value === 'null') return null
+      return cat.value
+    }
+
+    if (cat === 'null') return null
+
+    if (!mongoose.Types.ObjectId.isValid(cat)) return null
+
+    return cat
   }
+
+  // якщо масив
+  if (Array.isArray(categories)) {
+    const ids = categories.map(normalize).filter(Boolean)
+    return ids.length ? ids : undefined
+  }
+
+  // якщо одиночне значення
+  const id = normalize(categories)
+  return id ? [id] : undefined
 }
+
 module.exports = getCategoriesOptions
