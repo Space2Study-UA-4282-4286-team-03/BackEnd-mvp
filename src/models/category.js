754b5450073d19498338a92cb960956f972ceb33
navimov <108539100+navimov@users.noreply.github.com>
2026-02-24 18:00:33 +0200

    #98-implement category model
    * Created Category model, added CONSTS/MODELS.JS -> CATEGORY
    
    * Added swagger part of the task

---
+++ src/models/category.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose')
+const { CATEGORY } = require('~/consts/models')
+const { FIELD_CANNOT_BE_EMPTY, FIELD_CANNOT_BE_LONGER, FIELD_CANNOT_BE_SHORTER } = require('~/consts/errors')
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, FIELD_CANNOT_BE_EMPTY('name')],
+      minLength: [1, FIELD_CANNOT_BE_SHORTER('name', 1)],
+      maxLength: [30, FIELD_CANNOT_BE_LONGER('name', 30)],
+      unique: true
+    },
+    appearance: {
+      icon: {
+        type: String,
+        required: [true, FIELD_CANNOT_BE_EMPTY('appearance.icon')],
+        default: 'mocked-path-to-icon'
+      },
+      color: {
+        type: String,
+        required: [true, FIELD_CANNOT_BE_EMPTY('appearance.color')],
+        default: '#66C42C'
+      }
+    }
+  },
+  { timestamps: true, versionKey: false }
+)
+module.exports = model(CATEGORY, categorySchema)
